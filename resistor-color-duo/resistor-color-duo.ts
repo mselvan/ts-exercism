c2ba65cefffe23e59e339445fb840a0d79e920ec
Manimaran Selvan <manimaran.selvan@aurea.com>
2023-02-10 12:28:47 +0530

    Resistor color submitted

--- resistor-color-duo/resistor-color-duo.ts
+++ resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,17 @@
-export function decodedValue(values: string[]) {
+export const COLORS: string[] = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white'
+];
 
-}
+export const decodedValue = (colorArr: string[]): number => {
+    const [first, second] = colorArr;
+    return parseInt(`${COLORS.indexOf(first)}${COLORS.indexOf(second)}`);
+};
---
+++ resistor-color/resistor-color.ts
@@ -0,0 +1,17 @@
+export const COLORS: string[] = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white'
+];
+export const colorCode = (color: string) => {
+    return COLORS.indexOf(color);
+}
+
+
